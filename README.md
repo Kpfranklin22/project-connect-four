20a6bf7e92448637353d7871027f4f56bc3e8ffe
Kyle Franklin <kpfranklin22@gmail.com>
2022-11-27 18:46:44 -0500

    finished with working winning text

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 This is a computerized version of the popular board game Connect Four made with HTML, CSS, and Javascript.
 
+Deployed website: https://kpfranklin22.github.io/project-connect-four/
+
 ## Usage
 
 Once on the page you can play the game Connect Four and once you achieve the winning combo
--- app.js
+++ app.js
@@ -1,148 +1,37 @@
 // DOM Elements
-const allCells = document.querySelectorAll(".cell:not(.row-top)");
-const topCells = document.querySelectorAll(".cell.row-top");
-const resetButton = document.querySelector(".reset");
-const statusSpan = document.querySelector(".status");
+const allCells = document.querySelectorAll('.cell:not(.row-top)');
+const topCells = document.querySelectorAll('.cell.row-top');
+const resetButton = document.querySelector('.reset');
+const statusSpan = document.querySelector('.status');
 
 // columns
-const column0 = [
-  allCells[35],
-  allCells[28],
-  allCells[21],
-  allCells[14],
-  allCells[7],
-  allCells[0],
-  topCells[0],
-];
-const column1 = [
-  allCells[36],
-  allCells[29],
-  allCells[22],
-  allCells[15],
-  allCells[8],
-  allCells[1],
-  topCells[1],
-];
-const column2 = [
-  allCells[37],
-  allCells[30],
-  allCells[23],
-  allCells[16],
-  allCells[9],
-  allCells[2],
-  topCells[2],
-];
-const column3 = [
-  allCells[38],
-  allCells[31],
-  allCells[24],
-  allCells[17],
-  allCells[10],
-  allCells[3],
-  topCells[3],
-];
-const column4 = [
-  allCells[39],
-  allCells[32],
-  allCells[25],
-  allCells[18],
-  allCells[11],
-  allCells[4],
-  topCells[4],
-];
-const column5 = [
-  allCells[40],
-  allCells[33],
-  allCells[26],
-  allCells[19],
-  allCells[12],
-  allCells[5],
-  topCells[5],
-];
-const column6 = [
-  allCells[41],
-  allCells[34],
-  allCells[27],
-  allCells[20],
-  allCells[13],
-  allCells[6],
-  topCells[6],
-];
+const column0 = [allCells[35], allCells[28], allCells[21], allCells[14], allCells[7], allCells[0], topCells[0]];
+const column1 = [allCells[36], allCells[29], allCells[22], allCells[15], allCells[8], allCells[1], topCells[1]];
+const column2 = [allCells[37], allCells[30], allCells[23], allCells[16], allCells[9], allCells[2], topCells[2]];
+const column3 = [allCells[38], allCells[31], allCells[24], allCells[17], allCells[10], allCells[3], topCells[3]];
+const column4 = [allCells[39], allCells[32], allCells[25], allCells[18], allCells[11], allCells[4], topCells[4]];
+const column5 = [allCells[40], allCells[33], allCells[26], allCells[19], allCells[12], allCells[5], topCells[5]];
+const column6 = [allCells[41], allCells[34], allCells[27], allCells[20], allCells[13], allCells[6], topCells[6]];
 const columns = [column0, column1, column2, column3, column4, column5, column6];
 
+
 // rows
-const topRow = [
-  topCells[0],
-  topCells[1],
-  topCells[2],
-  topCells[3],
-  topCells[4],
-  topCells[5],
-  topCells[6],
-];
-const row0 = [
-  allCells[0],
-  allCells[1],
-  allCells[2],
-  allCells[3],
-  allCells[4],
-  allCells[5],
-  allCells[6],
-];
-const row1 = [
-  allCells[7],
-  allCells[8],
-  allCells[9],
-  allCells[10],
-  allCells[11],
-  allCells[12],
-  allCells[13],
-];
-const row2 = [
-  allCells[14],
-  allCells[15],
-  allCells[16],
-  allCells[17],
-  allCells[18],
-  allCells[19],
-  allCells[20],
-];
-const row3 = [
-  allCells[21],
-  allCells[22],
-  allCells[23],
-  allCells[24],
-  allCells[25],
-  allCells[26],
-  allCells[27],
-];
-const row4 = [
-  allCells[28],
-  allCells[29],
-  allCells[30],
-  allCells[31],
-  allCells[32],
-  allCells[33],
-  allCells[34],
-];
-const row5 = [
-  allCells[35],
-  allCells[36],
-  allCells[37],
-  allCells[38],
-  allCells[39],
-  allCells[40],
-  allCells[41],
-];
+const topRow = [topCells[0], topCells[1], topCells[2], topCells[3], topCells[4], topCells[5], topCells[6]];
+const row0 = [allCells[0], allCells[1], allCells[2], allCells[3], allCells[4], allCells[5], allCells[6]];
+const row1 = [allCells[7], allCells[8], allCells[9], allCells[10], allCells[11], allCells[12], allCells[13]];
+const row2 = [allCells[14], allCells[15], allCells[16], allCells[17], allCells[18], allCells[19], allCells[20]];
+const row3 = [allCells[21], allCells[22], allCells[23], allCells[24], allCells[25], allCells[26], allCells[27]];
+const row4 = [allCells[28], allCells[29], allCells[30], allCells[31], allCells[32], allCells[33], allCells[34]];
+const row5 = [allCells[35], allCells[36], allCells[37], allCells[38], allCells[39], allCells[40], allCells[41]];
 const rows = [row0, row1, row2, row3, row4, row5, topRow];
 
-// Variables
 
+// variables
 let gameIsLive = true;
 let yellowIsNext = true;
 
-// Functions
 
+// Functions
 const getClassListArray = (cell) => {
   const classList = cell.classList;
   return [...classList];
@@ -151,8 +40,8 @@ const getClassListArray = (cell) => {
 const getCellLocation = (cell) => {
   const classList = getClassListArray(cell);
 
-  const rowClass = classList.find((className) => className.includes("row"));
-  const colClass = classList.find((className) => className.includes("col"));
+  const rowClass = classList.find(className => className.includes('row'));
+  const colClass = classList.find(className => className.includes('col'));
   const rowIndex = rowClass[4];
   const colIndex = colClass[4];
   const rowNumber = parseInt(rowIndex, 10);
@@ -167,23 +56,24 @@ const getFirstOpenCellForColumn = (colIndex) => {
 
   for (const cell of columnWithoutTop) {
     const classList = getClassListArray(cell);
-    if (!classList.includes("yellow") && !classList.includes("red")) {
+    if (!classList.includes('yellow') && !classList.includes('red')) {
       return cell;
     }
   }
+
   return null;
 };
 
 const clearColorFromTop = (colIndex) => {
   const topCell = topCells[colIndex];
-  topCell.classList.remove("yellow");
-  topCell.classList.remove("red");
+  topCell.classList.remove('yellow');
+  topCell.classList.remove('red');
 };
 
 const getColorOfCell = (cell) => {
   const classList = getClassListArray(cell);
-  if (classList.includes("yellow")) return "yellow";
-  if (classList.includes("red")) return "red";
+  if (classList.includes('yellow')) return 'yellow';
+  if (classList.includes('red')) return 'red';
   return null;
 };
 
@@ -192,9 +82,9 @@ const checkWinningCells = (cells) => {
 
   gameIsLive = false;
   for (const cell of cells) {
-    cell.classList.add("win");
+    cell.classList.add('win');
   }
-  statusSpan.textContent = `${yellowisNext ? "Yellow" : "Red"} has won!`;
+  statusSpan.textContent = `${yellowIsNext ? 'Yellow' : 'Red'} has won!`
   return true;
 };
 
@@ -203,7 +93,7 @@ const checkStatusOfGame = (cell) => {
   if (!color) return;
   const [rowIndex, colIndex] = getCellLocation(cell);
 
-  // check horizontally
+  // Check horizontally
   let winningCells = [cell];
   let rowToCheck = rowIndex;
   let colToCheck = colIndex - 1;
@@ -211,7 +101,7 @@ const checkStatusOfGame = (cell) => {
     const cellToCheck = rows[rowToCheck][colToCheck];
     if (getColorOfCell(cellToCheck) === color) {
       winningCells.push(cellToCheck);
-      coltoCheck--;
+      colToCheck--;
     } else {
       break;
     }
@@ -226,9 +116,9 @@ const checkStatusOfGame = (cell) => {
       break;
     }
   }
-
   let isWinningCombo = checkWinningCells(winningCells);
   if (isWinningCombo) return;
+
 
   // Check vertically
   winningCells = [cell];
@@ -255,6 +145,7 @@ const checkStatusOfGame = (cell) => {
   }
   isWinningCombo = checkWinningCells(winningCells);
   if (isWinningCombo) return;
+
 
   // Check diagonally /
   winningCells = [cell];
@@ -284,6 +175,7 @@ const checkStatusOfGame = (cell) => {
   }
   isWinningCombo = checkWinningCells(winningCells);
   if (isWinningCombo) return;
+
 
   // Check diagonally \
   winningCells = [cell];
@@ -319,7 +211,7 @@ const checkStatusOfGame = (cell) => {
   for (const row of rowsWithoutTop) {
     for (const cell of row) {
       const classList = getClassListArray(cell);
-      if (!classList.includes("yellow") && !classList.includes("red")) {
+      if (!classList.includes('yellow') && !classList.includes('red')) {
         return;
       }
     }
@@ -329,15 +221,16 @@ const checkStatusOfGame = (cell) => {
   statusSpan.textContent = "Game is a tie!";
 };
 
-// Event handlers
 
+
+// Event Handlers
 const handleCellMouseOver = (e) => {
   if (!gameIsLive) return;
   const cell = e.target;
   const [rowIndex, colIndex] = getCellLocation(cell);
 
   const topCell = topCells[colIndex];
-  topCell.classList.add(yellowIsNext ? "yellow" : "red");
+  topCell.classList.add(yellowIsNext ? 'yellow' : 'red');
 };
 
 const handleCellMouseOut = (e) => {
@@ -355,36 +248,38 @@ const handleCellClick = (e) => {
 
   if (!openCell) return;
 
-  openCell.classList.add(yellowIsNext ? "yellow" : "red");
+  openCell.classList.add(yellowIsNext ? 'yellow' : 'red');
   checkStatusOfGame(openCell);
 
   yellowIsNext = !yellowIsNext;
   clearColorFromTop(colIndex);
   if (gameIsLive) {
     const topCell = topCells[colIndex];
-    topCell.classList.add(yellowIsNext ? "yellow" : "red");
+    topCell.classList.add(yellowIsNext ? 'yellow' : 'red');
   }
 };
 
-// Adding Event Listeners
 
+
+
+// Adding Event Listeners
 for (const row of rows) {
   for (const cell of row) {
-    cell.addEventListener("mouseover", handleCellMouseOver);
-    cell.addEventListener("mouseout", handleCellMouseOut);
-    cell.addEventListener("click", handleCellClick);
+    cell.addEventListener('mouseover', handleCellMouseOver);
+    cell.addEventListener('mouseout', handleCellMouseOut);
+    cell.addEventListener('click', handleCellClick);
   }
 }
 
-resetButton.addEventListener("click", () => {
+resetButton.addEventListener('click', () => {
   for (const row of rows) {
     for (const cell of row) {
-      cell.classList.remove("red");
-      cell.classList.remove("yellow");
-      cell.classList.remove("win");
+      cell.classList.remove('red');
+      cell.classList.remove('yellow');
+      cell.classList.remove('win');
     }
   }
   gameIsLive = true;
   yellowIsNext = true;
-  statusSpan.textContent = "";
+  statusSpan.textContent = '';
 });
